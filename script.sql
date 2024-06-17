41f023c66f8186344f07e4e093c17ef224a543d6
Artur Stein <arturstein2004@gmail.com>
2024-06-17 18:51:52 -0300

    feat(ap13): finaliza ex 1.4.2

--- script.sql
+++ script.sql
@@ -34,3 +34,33 @@ BEGIN
     RAISE NOTICE 'Total de pessoas maiores de idade: %', resultado;
 END;
 $$
+
+-- 1.4.2 Exibe o percentual de estudantes de cada sexo.
+
+CREATE OR REPLACE PROCEDURE sp_percentual_sexo(OUT resultado_F NUMERIC(10,2), OUT resultado_M NUMERIC(10,2))
+LANGUAGE plpgsql
+AS $$ 
+BEGIN
+    SELECT (COUNT(gender) * 100.0) / (SELECT COUNT(gender) FROM tb_students) AS porcentagem_F
+    FROM tb_students 
+    WHERE gender = 1
+    INTO resultado_F;
+
+    SELECT (COUNT(gender) * 100.0) / (SELECT COUNT(gender) FROM tb_students) AS percentagem_M
+    FROM tb_students 
+    WHERE gender = 2
+    INTO resultado_M;
+
+END;
+$$
+
+DO $$
+DECLARE 
+    resultado_F NUMERIC(10,2);
+    resultado_M NUMERIC(10,2);
+BEGIN
+    CALL sp_percentual_sexo(resultado_F, resultado_M);
+    RAISE NOTICE 'Total percentual de pessoas do sexo feminino: %, e do sexo masculino: %', 
+    resultado_F, resultado_M;
+END;
+$$
